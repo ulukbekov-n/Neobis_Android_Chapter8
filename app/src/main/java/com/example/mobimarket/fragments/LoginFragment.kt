8360b2ebdf8028d5a5dd7475c428de695f47e57c
Ulukbekov Naken <ulukbekov_n@auca.kg>
2023-06-21 18:52:13 +0600

    login fragment

--- app/src/main/java/com/example/mobimarket/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/mobimarket/fragments/LoginFragment.kt
@@ -2,22 +2,75 @@ package com.example.mobimarket.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.mobimarket.R
 import com.example.mobimarket.databinding.LoginFragmentBinding
-import com.example.mobimarket.databinding.MainFragmentBinding
+
 
 class LoginFragment:Fragment() {
     private lateinit var binding: LoginFragmentBinding
+    lateinit var editTextUserName: EditText
+    lateinit var editTextPassword: EditText
+    lateinit var button: Button
+    lateinit var textWatcher: TextWatcher
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = LoginFragmentBinding.inflate(inflater, container, false)
+        editTextUserName = binding.loginUserName
+        editTextPassword = binding.loginPassword
+        button = binding.loginButton
+        textWatcher = object:TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                val userName = editTextUserName.text.toString().trim()
+                val password = editTextPassword.text.toString().trim()
+                val enableButton = userName.isNotEmpty() && password.isNotEmpty()
+                button.isEnabled = enableButton
+                if (enableButton) {
+                    button.setBackgroundResource(R.drawable.enabled_back)
+
+                } else {
+                    button.setBackgroundResource(R.drawable.back )
+                }
+            }
+
+        }
+        binding.loginButton.setOnClickListener {
+            val userName = editTextUserName.text.toString().trim()
+            val password = editTextPassword.text.toString().trim()
+
+            if (userName.isEmpty() || password.isEmpty()) {
+                // Display a warning message here, e.g., a Toast
+                Toast.makeText(context, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                // Perform the login action
+                findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+            }
+        }
+        editTextUserName.addTextChangedListener(textWatcher)
+        editTextPassword.addTextChangedListener(textWatcher)
         return binding.root
     }
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/example/mobimarket/fragments/RegisterFragment.kt
+++ app/src/main/java/com/example/mobimarket/fragments/RegisterFragment.kt
@@ -1,4 +1,21 @@
 package com.example.mobimarket.fragments
 
-class RegisterFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mobimarket.databinding.RegisterFragmentBinding
+
+class RegisterFragment : Fragment() {
+    private lateinit var binding:RegisterFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = RegisterFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
 }
